dcd9635cba165e149e44dbfbcff93e8a09310e86
Jason Sutanto <jasonsutanto@Jasons-MacBook-Pro.local>
2021-07-13 13:39:09 -0500

    changed explore page

--- screens/TranslateScreen.js
+++ screens/TranslateScreen.js
@@ -1,7 +1,9 @@
 import React, {useState} from 'react';
-import { StyleSheet, Text, View, TextInput } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Alert, ScrollView } from 'react-native';
 import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
 import { Avatar, Button, Card, Title, Paragraph } from 'react-native-paper';
+import FormButton from '../components/FormButton';
+
 //import fetch from 'node-fetch';
 /*
 const API_TOKEN = "api_XjRRyCXAXqfXdhYMTqqxjAkqtLCrlhWVJu";
@@ -29,30 +31,39 @@ query("Can you please let us know more details about your ").then((response) => 
 // hard drive. It shows your current/past usage of the device(s)"}]
 */
 
-const data = "Can you please let us know more details about your ";
-const API_TOKEN = "api_XjRRyCXAXqfXdhYMTqqxjAkqtLCrlhWVJu";
-const url = "https://api-inference.huggingface.co/models/gpt2";
-const axios = require('axios').default;
 
-const config = {
-    headers: { Authorization: `Bearer ${API_TOKEN}` }
-};
-
-axios.post("https://api-inference.huggingface.co/models/gpt2", data, config)
-    .then(function (response) {
-        console.log(response.data);
-    })
-    .catch(function (error) {
-        console.log(error);
-    });
 
 
 const TranslateScreen = () => {
-    const [input, setInput] = useState("");
+    var [input, setInput] = useState("");
+
+    const handleTranslate = () => {
+
+        const data = input;
+
+        const API_TOKEN = "api_XjRRyCXAXqfXdhYMTqqxjAkqtLCrlhWVJu";
+        const url = "https://api-inference.huggingface.co/models/gpt2";
+        const axios = require('axios').default;
+
+        const config = {
+            headers: { Authorization: `Bearer ${API_TOKEN}` }
+        };
+
+        axios.post("https://api-inference.huggingface.co/models/gpt2", data, config)
+            .then(function (response) {
+                console.log(response.data[0].generated_text);
+                setInput(response.data[0].generated_text)
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    }
+
 
     return (
         <View style={styles.container}>
-            <TextInput style={{borderWidth:1, height:150, width: 300, padding: 10, marginBottom: 20, borderRadius: 10, fontFamily: 'Cochin', justifyContent:'center', alignItems: 'center'}} placeholder="Type in a sentence" onChangeText={(text)=>setInput(text)}/>
+            <TextInput style={{borderWidth:1, height:150, width: 300, padding: 10, marginBottom: 20, borderRadius: 10, fontFamily: 'Cochin', justifyContent:'center', alignItems: 'center'}} placeholder="Type in a sentence"/>
+            <FormButton buttonTitle="Translate" onPress={handleTranslate}/>
             <View style={styles.container1}>
                 <FontAwesome5
                     name="arrow-up"
@@ -65,10 +76,15 @@ const TranslateScreen = () => {
                     backgroundColor="#fff"
                 />
             </View>
-
-            <View style={styles.card}>
-                <View style={styles.cardContent}><Text style={{fontFamily: 'Cochin'}}>{input}</Text></View>
-            </View>
+            
+            <ScrollView>
+                <View style={styles.card}>
+                    <View style={styles.cardContent}>
+                        <Text style={{fontFamily: 'Cochin'}}>{input}</Text>
+                    </View>
+                </View>
+            </ScrollView>
+            
             
         </View>
     )
